ec3d7b158bb892145553f8dde5545f96ab211590
Angulo Florian <fangulo@localhost.localdomain>
2019-04-09 11:08:09 +0200

    deriveeee

--- TraitementImageGuillemain/Donnees/SpxSLIC.m
+++ TraitementImageGuillemain/Donnees/SpxSLIC.m
@@ -6,9 +6,16 @@ function Msq = SpxSLIC(I,K)
 end
 
 function G = derivees(I)
-kernel_Sobel =   0.25*[1 2 1;
-            0 0 1;
+kernel_Sobel_y =   0.25*[1 2 1;
+            0 0 0;
            -1 -2 -1];
+kernel_Sobel_x = 0.25*[1 0 -1;
+                       2 0 -2;
+                       1 0 -1];
+       
+Gx = conv(I,kernel_Sobel_x,'same');
+Gy = conv(I,kernel_Sobel_y,'same');
 
+G = sqrt(Gx.^2 + Gy^2);
     
 end
