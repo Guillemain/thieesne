df943bc69fd480596d36905d21ace0c6aa46469c
Guillemain <antoine.guillemain@gmail.com>
2019-04-09 20:53:50 +0200

    hiuh

--- TAV/TP9/U_AD.m
+++ TAV/TP9/U_AD.m
@@ -2,6 +2,6 @@ function [U] = U_AD(R,X,Y,I)
 % Calcul la valeur de l'attache aux données d'un cercle.
 gamma = 5.0;
 S = 130;
-U = sum(1-2/(1+exp(-gamma *((nvg_moyen(x,y,R,I))/(S) - 1))));
+U = sum(1-2/(1+exp(-gamma *((nvg_moyen(X,Y,R,I))/(S) - 1))));
 end
 
--- TAV/TP9/recouvrement.m
+++ TAV/TP9/recouvrement.m
@@ -1,4 +1,5 @@
 function Recou = recouvrement(X,Y,x_d,y_d,R_2)
-Recou = Recou + sum( ((x_d - X).^2 + (y_d - Y).^2)) <= R_2*2 );
+Dist = (x_d - X).^2 + (y_d - Y).^2;
+Recou = sum( (  Dist <= R_2*2));
 end
 
