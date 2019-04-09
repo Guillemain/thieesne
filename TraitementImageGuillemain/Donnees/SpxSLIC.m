f29ae6d9b3c14af74944d1cfee077fd203eea91f
Guillemain <aguillem3@localhost.localdomain>
2019-04-09 11:09:01 +0200

    dj

--- TraitementImageGuillemain/Donnees/SpxSLIC.m
+++ TraitementImageGuillemain/Donnees/SpxSLIC.m
@@ -2,7 +2,19 @@ function Msq = SpxSLIC(I,K)
 %
 %
 
-    
+% Le gradient de notre image.
+Grad = derivees(I);
+
+% Le pas de la grille.
+sy = floor(size(I,2)/K); sx = floor(size(I,1)/K);
+
+for i = 1:sx:size(I,1)-sx
+   for j = 1:sy:size(I,2)-xy
+      
+       [~,centre_minimiseur] = min(Grad(i:i+sx,j:j+sy)); % On cherche pour chaque bloc les centre minimiseur du gradient.
+       
+   end
+end
 end
 
 function G = derivees(I)
